4f89360b5cffe3bca5476770581a1d97b6a67cf0
Ranjithlevine <101319595+Ranjithlevine@users.noreply.github.com>
2024-08-18 17:41:41 +0530

    Add files via upload

---
+++ src/AutomationTask/Count_integer.java
@@ -0,0 +1,20 @@
+package AutomationTask;
+
+import java.util.Scanner;
+
+public class Count_integer {
+    public static void main(String[] Argd) {
+        Scanner sc=new Scanner(System.in);
+        int Value=sc.nextInt();
+        int count=0;
+       do {
+       Value=Value/10;
+       count++;
+       }
+       while (Value!=0);
+       System.out.println("The Count of the given digit is "+count);
+       }
+
+
+    }
+
